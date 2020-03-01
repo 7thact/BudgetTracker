e0d6bcb9071d4d5ff132a50ee766201b6af9132a
Charles Cressey <cmcressey@gmail.com>
2020-03-01 00:33:34 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # BudgetTracker
+
+Technologies Used:
+compression, express, and mongoose
+
+Directions for use:
+
+Do a npm install for compression, express, and mongoose.
+
+First, use the add funds botton on the left to add money to start the tracker.
+
+Input funds to subtract from the initial value to track expenses.
+
+Track your expenses on and offline anytime!
+
+Deployed Site:
+https://calm-brushlands-15442.herokuapp.com/
--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,7 @@ const FILES_TO_CACHE = [
     const CACHE_NAME = "static-cache-v2";
     const DATA_CACHE_NAME = "data-cache-v1";
     
-    // install phase
+   
     self.addEventListener("install", function(evt) {
       evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -17,11 +17,11 @@ const FILES_TO_CACHE = [
           return cache.addAll(FILES_TO_CACHE);
         })
       );
-    // go directly into activation phase
+   
       self.skipWaiting();
     });
     
-    // activate phase
+   
     self.addEventListener("activate", function(evt) {
       evt.waitUntil(
         caches.keys().then(keyList => {
@@ -39,15 +39,14 @@ const FILES_TO_CACHE = [
       self.clients.claim();
     });
     
-    // fetch
+    
     self.addEventListener("fetch", function(evt) {
-      // cache successful requests to the API
+
       if (evt.request.url.includes("/api/")) {
         evt.respondWith(
           caches.open(DATA_CACHE_NAME).then(cache => {
             return fetch(evt.request)
               .then(response => {
-                // If the response was good, clone it and store it in the cache.
                 if (response.status === 200) {
                   cache.put(evt.request.url, response.clone());
                 }
@@ -55,7 +54,6 @@ const FILES_TO_CACHE = [
                 return response;
               })
               .catch(err => {
-                // Network request failed, try to get it from the cache.
                 return cache.match(evt.request);
               });
           }).catch(err => console.log(err))
@@ -64,7 +62,6 @@ const FILES_TO_CACHE = [
         return;
       }
     
-      // if the request is not for the API, serve static assets using "offline-first" approach.
       evt.respondWith(
         caches.match(evt.request).then(function(response) {
           return response || fetch(evt.request);
